692fef2c2f394ef7e77c5ff21e289e1a145d1107
Dan <dpgraham.85@gmail.com>
2020-11-21 21:12:34 -0800

    again

--- OBSERVATIONS.md
+++ OBSERVATIONS.md
@@ -12,3 +12,18 @@
 * Alert takes 81 seconds to dismiss
 
 ## Appium v1.19.0
+
+# Target ID on VM
+
+Runs | Has Error
+=================
+1   | true
+2   | false
+3   | false
+4   | false
+5   | false
+6   | false
+7   | 
+8
+9
+10
--- alert-repro.js
+++ alert-repro.js
@@ -3,33 +3,38 @@ const B = require('bluebird');
 const { retryInterval } = require('asyncbox');
 
 (async function () {
-  
-    let driver = await wd.promiseChainRemote({host: 'localhost', port: 4723});
-    try {
-        await driver.init({
-            platformName: 'iOS',
-            deviceName: 'iPhone 11 Pro Max',
-            platformVersion: process.env.PLATFORM_VERSION || '13.4',
-            automationName: 'XCUITest',
-            browserName: 'safari',
-            safariAllowPopups: true,
-            safariLogAllCommunication: true,
-        });
+    let failureCount = 0;
+    for (let i=0; i<100; i++) {
+        let driver = await wd.promiseChainRemote({host: 'localhost', port: 4723});
+        try {
+            await driver.init({
+                platformName: 'iOS',
+                deviceName: 'iPhone 11 Pro Max',
+                platformVersion: process.env.PLATFORM_VERSION || '13.4',
+                automationName: 'XCUITest',
+                browserName: 'safari',
+                safariAllowPopups: true,
+                safariLogAllCommunication: true,
+            });
 
-        await driver.get("https://qavbox.github.io/demo/signup/");
-        await B.delay(2000);
-        console.log(await driver.source());
-        const submitBtn = await driver.elementById('submit');
-        await submitBtn.click();
-        const startTime = +(new Date());
-        await retryInterval(5, 500, () => driver.acceptAlert());
-        const duration = ((+(new Date())) - startTime) / 1000;
-        console.log(`It took '${duration}' seconds to accept`);
-        await B.delay(1000);
-    } catch(e) {
-        console.error(e);
-    } finally {
-        await driver.deleteSession();
+            await driver.get("https://qavbox.github.io/demo/signup/");
+            await B.delay(2000);
+            console.log(await driver.source());
+            const submitBtn = await driver.elementById('submit');
+            await submitBtn.click();
+            const startTime = +(new Date());
+            await retryInterval(5, 500, () => driver.acceptAlert());
+            const duration = ((+(new Date())) - startTime) / 1000;
+            console.log(`It took '${duration}' seconds to accept`);
+            await B.delay(1000);
+        } catch(e) {
+            failureCount++;
+            console.error(e);
+        } finally {
+            await driver.deleteSession();
+        }
     }
+
+    console.log(`=============FAILED '${failureCount}' TIMES==============`);
 
 })();
