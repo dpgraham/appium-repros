8b277f420e660bf7dc973a89f070d5a44cb1476f
Dan <dpgraham.85@gmail.com>
2020-11-21 21:13:31 -0800

    again

--- target-id.js
+++ target-id.js
@@ -3,26 +3,29 @@ const B = require('bluebird');
 const { retryInterval } = require('asyncbox');
 
 (async function () {
-  
-    let driver = await wd.promiseChainRemote({host: 'localhost', port: 4723});
-    try {
-        await driver.init({
-            platformName: 'iOS',
-            deviceName: 'iPhone 11 Pro Max',
-            platformVersion: process.env.PLATFORM_VERSION || '13.4',
-            automationName: 'XCUITest',
-            browserName: 'safari',
-            safariAllowPopups: true,
-            safariLogAllCommunication: true,
-        });
+    let failureCount = 0;
+    for (let i=0; i<100; i++) {
+        let driver = await wd.promiseChainRemote({host: 'localhost', port: 4723});
+        try {
+            await driver.init({
+                platformName: 'iOS',
+                deviceName: 'iPhone 11 Pro Max',
+                platformVersion: process.env.PLATFORM_VERSION || '13.4',
+                automationName: 'XCUITest',
+                browserName: 'safari',
+                safariAllowPopups: true,
+                safariLogAllCommunication: true,
+            });
 
-        await driver.get('http://alpha.realtor.com/realestateandhomes-search/Los-Angeles_CA?ab_vst=SWIPE_C&ads=0&tracking=0&split_tcv=157');
-        await B.delay(1000);
-        console.log(await driver.source());
-    } catch(e) {
-        console.error(e);
-    } finally {
-        await driver.quit();
+            await driver.get('http://alpha.realtor.com/realestateandhomes-search/Los-Angeles_CA?ab_vst=SWIPE_C&ads=0&tracking=0&split_tcv=157');
+            await B.delay(1000);
+            console.log(await driver.source());
+        } catch(e) {
+            console.error(e);
+        } finally {
+            await driver.quit();
+        }
     }
+    console.log(`=============FAILED '${failureCount}' TIMES==============`);
 
 })();
