e484e6146f9ec0f1f0cf3f0ae95ee464885313a5
Dan <dpgraham.85@gmail.com>
2020-11-22 15:52:56 -0800

    Again

--- alert-repro.js
+++ alert-repro.js
@@ -24,7 +24,7 @@ const { retryInterval } = require('asyncbox');
             await submitBtn.click();
             const startTime = +(new Date());
             //const text = await retryInterval(5, 500, () => driver.alertText());
-            console.log('####TEXT', text);  
+            //console.log('####TEXT', text);  
             await retryInterval(5, 500, () => driver.acceptAlert());
             const duration = ((+(new Date())) - startTime) / 1000;
             console.log(`It took '${duration}' seconds to accept`);
